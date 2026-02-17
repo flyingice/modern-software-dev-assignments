a0cf72cc132efa4306304605ead4fb9ac3f79df5
flyingice <flyingice.eth@outlook.com>
2026-02-16 20:43:08 -0800

    implement retrieval-augmented generation

--- week1/rag.py
+++ week1/rag.py
@@ -36,8 +36,7 @@ QUESTION = (
 )
 
 
-# TODO: Fill this in!
-YOUR_SYSTEM_PROMPT = ""
+YOUR_SYSTEM_PROMPT = "You are an expert software engineer."
 
 
 # For this simple example
@@ -52,11 +51,10 @@ REQUIRED_SNIPPETS = [
 
 
 def YOUR_CONTEXT_PROVIDER(corpus: List[str]) -> List[str]:
-    """TODO: Select and return the relevant subset of documents from CORPUS for this task.
-
+    """
     For example, return [] to simulate missing context, or [corpus[0]] to include the API docs.
     """
-    return []
+    return [s for s in corpus if "api" in s.lower()]
 
 
 def make_user_prompt(question: str, context_docs: List[str]) -> str:
